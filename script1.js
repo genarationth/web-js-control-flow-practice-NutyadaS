141d05de9c405a5e9fe9705cc98b336f7f04baac
NutyadaS <nutyada.s7810@gmail.com>
2023-03-13 14:21:49 +0700

    add Switch

---
+++ script1.js
@@ -0,0 +1,26 @@
+let dayNumbers = Math.floor(Math.random() * 7);
+switch (dayNumbers) {
+  case (dayNumbers = 0):
+    console.log("Sunday");
+    break;
+  case (dayNumbers = 1):
+    console.log("Monday");
+    break;
+  case (dayNumbers = 2):
+    console.log("Tuesday");
+    break;
+  case (dayNumbers = 3):
+    console.log("Wednesday");
+    break;
+  case (dayNumbers = 4):
+    console.log("Thursday");
+    break;
+  case (dayNumbers = 5):
+    console.log("Friday");
+    break;
+  case (dayNumbers = 6):
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid day number");
+}
